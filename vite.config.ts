9e52b616b404eb767ff59440f0246c9b70d347fc
Joe Chung <joechung2008@users.noreply.github.com>
2025-09-03 00:14:19 -0400

    Use manualChunks to split HeroUI into smaller chunks

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,26 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/@heroui/button")) {
+            return "heroui-button";
+          } else if (id.includes("node_modules/@heroui/dropdown")) {
+            return "heroui-dropdown";
+          } else if (id.includes("node_modules/@heroui/navbar")) {
+            return "heroui-navbar";
+          } else if (id.includes("node_modules/@heroui/table")) {
+            return "heroui-table";
+          } else if (id.includes("node_modules/@heroui/tabs")) {
+            return "heroui-tabs";
+          } else {
+            return null;
+          }
+        },
+      },
+    },
+  },
   plugins: [react()],
 });
